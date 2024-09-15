f59dd258b20fcb754712b691497dfe86363558be
Shakil Ahmed <shakil200409@gmail.com>
2024-09-15 22:40:30 +0600

    tried includes method

---
+++ books.js
@@ -0,0 +1,8 @@
+const books = ['Feluda', 'Boomkesh', 'Sharlok Holmes', 'Chander Pahar'];
+
+if(books.includes('Javascript') === true){
+    console.log("The Javascript is available");
+}
+else{
+    console.log("The Javascript book is not available.")
+}
