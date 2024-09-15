f5cdd679e4ba9f0d3a04a21a651a74336ad95583
Shakil Ahmed <shakil200409@gmail.com>
2024-09-15 21:56:27 +0600

    first commit

---
+++ fruits.js
@@ -0,0 +1,11 @@
+const fruits = ['Aam', 'Jam', 'Kathal', 'Peyara', 'Boroi', 'Angur'];
+// Print any index fruits
+console.log(fruits[3]);
+
+console.log(fruits);
+
+// Change the fruit of an index 
+fruits[2] = 'Jambura';
+
+console.log(fruits);
+
