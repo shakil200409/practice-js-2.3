7c77f43417434cf571ee570d93dff2e031e75d59
Shakil Ahmed <shakil200409@gmail.com>
2024-09-15 22:03:48 +0600

    Tried to add and remove strings from an array

---
+++ destination.js
@@ -0,0 +1,16 @@
+const tourPlaces = ["Cox's Bazar", "Jafflong", "Saint Martin"];
+
+// Add a tourist place in array
+tourPlaces.push("Bandarban");
+
+console.log(tourPlaces);
+
+// Add more tourist places in array 
+tourPlaces.push("Bisanakandi", "Sundarban");
+
+console.log(tourPlaces);
+
+// Remove place from an array
+tourPlaces.pop();
+
+console.log(tourPlaces);
